ba110b33e8a86944e206c27d88d2e5335a9d3fa8
Tom Dietrich <tom.dietrich@gmail.com>
2013-10-24 11:29:07 -0400

    Style fix

--- AxBcWrapper/Query/ColumnProjector.cs
+++ AxBcWrapper/Query/ColumnProjector.cs
@@ -6,54 +6,40 @@ namespace AxBcWrapper.Query
 {
     internal class ColumnProjector : ExpressionVisitor
     {
-        #region Constants and Fields
-
-        private static MethodInfo miGetValue;
-        private int iColumn;
-        private ParameterExpression row;
-        private StringBuilder sb;
-
-        #endregion
-
-        #region Constructors and Destructors
+        private static MethodInfo _miGetValue;
+        private int _iColumn;
+        private ParameterExpression _row;
+        private StringBuilder _sb;
 
         internal ColumnProjector()
         {
-            if (miGetValue == null)
+            if (_miGetValue == null)
             {
-                miGetValue = typeof(ProjectionRow).GetMethod("GetValue");
+                _miGetValue = typeof(ProjectionRow).GetMethod("GetValue");
             }
         }
 
-        #endregion
-
-        #region Methods
-
         internal ColumnProjection ProjectColumns(Expression expression, ParameterExpression row)
         {
-            sb = new StringBuilder();
-            this.row = row;
-            Expression selector = Visit(expression);
-            return new ColumnProjection { Columns = sb.ToString(), Selector = selector };
+            _sb = new StringBuilder();
+            _row = row;
+            var selector = Visit(expression);
+            return new ColumnProjection { Columns = _sb.ToString(), Selector = selector };
         }
 
         protected override Expression VisitMemberAccess(MemberExpression m)
         {
             if (m.Expression != null && m.Expression.NodeType == ExpressionType.Parameter)
             {
-                if (sb.Length > 0)
+                if (_sb.Length > 0)
                 {
-                    sb.Append(", ");
+                    _sb.Append(", ");
                 }
-                sb.Append(m.Member.Name);
-                return Expression.Convert(Expression.Call(row, miGetValue, Expression.Constant(iColumn++)), m.Type);
+                _sb.Append(m.Member.Name);
+                return Expression.Convert(Expression.Call(_row, _miGetValue, Expression.Constant(_iColumn++)), m.Type);
             }
-            else
-            {
-                return base.VisitMemberAccess(m);
-            }
+            
+            return base.VisitMemberAccess(m);
         }
-
-        #endregion
     }
 }
